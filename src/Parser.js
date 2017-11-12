c2be364e44daa3d0a58e3ab11254c31862d2d27d
Stanisław Chmiela <sjchmiela@gmail.com>
2017-11-12 22:45:42 +0100

    Read countries as ISO, reject unknown ones

--- src/Parser.js
+++ src/Parser.js
@@ -1,26 +1,60 @@
-import cheerio from 'cheerio';
-import trim from 'lodash/trim';
-import filter from 'lodash/filter';
-import concat from 'lodash/concat';
-import isEmpty from 'lodash/isEmpty';
+import cheerio from "cheerio";
+import trim from "lodash/trim";
+import filter from "lodash/filter";
+import concat from "lodash/concat";
+import isEmpty from "lodash/isEmpty";
+import uniq from "lodash/uniq";
+
+const countryToIso = {
+  AUSTRIA: "AT",
+  BELGIA: "BE",
+  BUŁGARIA: "BG",
+  CHORWACJA: "HR",
+  CYPR: "CY",
+  CZECHY: "CZ",
+  DANIA: "DK",
+  ESTONIA: "EE",
+  FINLANDIA: "FI",
+  FRANCJA: "FR",
+  GRECJA: "GR",
+  HISZPANIA: "ES",
+  HOLANDIA: "NL",
+  ISLANDIA: "IS",
+  LITWA: "LT",
+  MACEDONIA: "MK",
+  NIEMCY: "DE",
+  NORWEGIA: "NO",
+  PORTUGALIA: "PT",
+  RUMUNIA: "RO",
+  SLOWACJA: "SK",
+  SŁOWENIA: "SI",
+  SZWECJA: "SE",
+  TURCJA: "TR",
+  WĘGRY: "HU",
+  WŁOCHY: "IT",
+  "WIELKA  BRYTANIA": "GB",
+};
 
 function getParsedDocument(html) {
   return cheerio.load(html);
 }
 
 function checkIfRowIsAnAgreementRow(row) {
-  const cells = cheerio(row).children('td');
+  const cells = cheerio(row).children("td");
   return cells.length === 8 && /\d+/.test(cells.eq(1).text());
 }
 
 function getAgreementsRows(doc) {
-  return doc('.c-col tbody')
-    .find('tr')
+  return doc(".c-col tbody")
+    .find("tr")
     .filter((_, element) => checkIfRowIsAnAgreementRow(element));
 }
 
 function parseAgreementRow(row) {
-  const cells = cheerio(row).children('td').toArray().map(cell => cheerio(cell).text());
+  const cells = cheerio(row)
+    .children("td")
+    .toArray()
+    .map(cell => cheerio(cell).text());
   // Single cell format:
   // [ '' | country,
   // '465',
@@ -37,26 +71,36 @@ function parseAgreementRow(row) {
     startYear: cells[4],
     endYear: cells[5],
     coordinator: cells[6],
-    department: cells[7],
+    department: cells[7]
   };
 }
 
 function getCountry(row) {
-  return trim(row.children('td').eq(0).text());
+  return trim(
+    row
+      .children("td")
+      .eq(0)
+      .text()
+  );
 }
 
 function parseAgreementRows(rows) {
   let country = null;
+  const unknownCountries = [];
 
   let parsedRows = [];
 
   for (let i = 0; i < rows.length; i += 1) {
     const element = rows.eq(i);
 
-    // If the row has a non-empty country specified, update context.
     const currentCountry = getCountry(element);
+
     if (!isEmpty(currentCountry)) {
-      country = currentCountry;
+      country = countryToIso[currentCountry];
+
+      if (isEmpty(country)) {
+        unknownCountries.push(currentCountry);
+      }
     }
 
     const rowData = parseAgreementRow(element);
@@ -65,20 +109,24 @@ function parseAgreementRows(rows) {
     parsedRows = concat(parsedRows, [rowData]);
   }
 
+  if (!isEmpty(unknownCountries)) {
+    console.warn(`Unknown countries: ${uniq(unknownCountries)}.`);
+  }
+
   return parsedRows;
 }
 
-function pickStudentsAgreements(agreements, rejectingCountry) {
-  return filter(agreements, ({ country }) => country !== rejectingCountry);
+function pickStudentsAgreements(agreements) {
+  return filter(agreements, 'country');
 }
 
-function getStudentsAgreements(html, rejectingCountry) {
+function getStudentsAgreements(html) {
   const doc = getParsedDocument(html);
   const agreementsRows = getAgreementsRows(doc);
   const agreements = parseAgreementRows(agreementsRows);
-  return pickStudentsAgreements(agreements, rejectingCountry);
+  return pickStudentsAgreements(agreements);
 }
 
 export default {
-  getStudentsAgreements,
+  getStudentsAgreements
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import fs from 'fs';
-import request from 'request';
+import fs from "fs";
+import request from "request";
 
-import Parser from './Parser';
-import { agreementsUrl, rejectingCountry } from '../config';
+import Parser from "./Parser";
+import { agreementsUrl, rejectingCountry } from "../config";
 
 if (process.argv.length <= 2) {
-  console.log('Argument missing: path to the output file.');
+  console.log("Argument missing: path to the output file.");
   process.exit(1);
 }
 
@@ -16,12 +16,12 @@ console.log(`Downloading URL ${agreementsUrl}...`);
 request.get(agreementsUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     console.error(`Downloaded ${body.length} bytes, parsing...`);
-    const studentsAgreements = Parser.getStudentsAgreements(body, rejectingCountry);
-    console.log('Writing to file...');
+    const studentsAgreements = Parser.getStudentsAgreements(body);
+    console.log("Writing to file...");
     fs.writeFileSync(fileToWriteTo, JSON.stringify(studentsAgreements));
     console.log(`Results have been saved to \`${fileToWriteTo}\`.`);
   } else if (error) {
-    console.log('Error occurred: ', error);
+    console.log("Error occurred: ", error);
   } else {
     console.log(`Wrong status code: ${response.statusCode}, expected 200.`);
   }
